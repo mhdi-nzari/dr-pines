e0e791bec67f40c516ffd5e3b8ace9a7da0b16a5
mhdi-nzari <mahdi.nazari.gb008@gmail.com>
2021-12-08 18:30:22 +0330

    updated header and config owl-carousel

---
+++ Src/Scripts/customize.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $(".Intro-slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    rtl:true,
+    responsiveClass: true,
+    padding: 10,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+});
--- Src/index.js
+++ Src/index.js
@@ -2,5 +2,12 @@ import $ from "jquery";
 import "bootstrap";
 import bootstrap from "bootstrap";
 
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel";
+
+
+
+
+
+
+
+
+
